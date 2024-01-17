44e4356011ba51969c2ef7d8a0454ec8e56b4783
Marlon Mariano <marlon.mariano.pb@compasso.com.br>
2024-01-17 18:26:39 -0300

    Inserção de usuário com Post

--- src/main/java/com/compass/workshopmongo/resources/UserResource.java
+++ src/main/java/com/compass/workshopmongo/resources/UserResource.java
@@ -6,7 +6,9 @@ import com.compass.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -31,4 +33,27 @@ public class UserResource {
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
     }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO){
+        User user = service.fromDTO(userDTO);
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@RequestBody UserDTO userDTO, @PathVariable String id){
+        User user = service.fromDTO(userDTO);
+        user.setId(id);
+        user = service.update(user);
+        return ResponseEntity.ok().body(user);
+    }
+
 }
--- src/main/java/com/compass/workshopmongo/services/UserService.java
+++ src/main/java/com/compass/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.compass.workshopmongo.services;
 
 import com.compass.workshopmongo.domain.User;
+import com.compass.workshopmongo.dto.UserDTO;
 import com.compass.workshopmongo.repositories.UserRepository;
 import com.compass.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,30 @@ public class UserService {
             throw new ObjectNotFoundException("Objeto não encontrado");
         }
         return user;
+    }
+
+    public User insert(User user){
+        return repo.insert(user);
+    }
+
+    public User fromDTO(UserDTO user){
+        return new User(user.getId(), user.getName(), user.getEmail());
+    }
+
+    public User update(User user){
+        User newUser = findById(user.getId());
+        updateData(newUser, user);
+        return repo.save(newUser);
+    }
+
+    private void updateData(User newUser, User user){
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+    }
+
+    public void delete(String id){
+        findById(id);
+        repo.deleteById(id);
     }
 
 
