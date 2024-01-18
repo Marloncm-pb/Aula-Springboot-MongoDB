e2197cee7fa3d8aebe49ee7a7d7dc9cdfdfa56de
Marlon Mariano <marlon.mariano.pb@compasso.com.br>
2024-01-18 16:38:20 -0300

    Consultanto Post por id

---
+++ src/main/java/com/compass/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.compass.workshopmongo.services;
+
+import com.compass.workshopmongo.domain.Post;
+import com.compass.workshopmongo.domain.User;
+import com.compass.workshopmongo.repositories.PostRepository;
+import com.compass.workshopmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id){
+        Post post = repo.findById(id).orElse(null);
+        if (post == null){
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return post;
+    }
+}
